20c1a58aeb17f3b450208ecd2e19235075a96dae
James Dempsey <james.dempsey@csiro.au>
2020-01-27 12:32:44 +1100

    Add concurrency limit Scan for completed jobs first Correct reported concurrency count

--- askap_cutout_daemon.py
+++ askap_cutout_daemon.py
@@ -48,6 +48,8 @@ def parseargs():
                         default='smc_srcs_image_params.vot')
     parser.add_argument("-m", "--max_loops", help="The maximum number of processing loops the daemon will run.",
                         type=int, default=500)
+    parser.add_argument("-c", "--concurrency_limit", help="The maximum number of concurrent processes allowed to run.",
+                        type=int, default=12)
     args = parser.parse_args()
     return args
 
@@ -102,10 +104,11 @@ def build_map(image_params):
     return src_beam_map
 
 
-def job_loop(targets, status_folder, src_beam_map, active_ids, active_ms, remaining_array_ids, completed_srcs):
-    total_concurrency = 0
+def job_loop(targets, status_folder, src_beam_map, active_ids, active_ms, remaining_array_ids, completed_srcs, concurrency_limit):
+    rate_limited = False
     # Take a copy of the list to avoid issues when removing items from it
     ids_to_scan = list(remaining_array_ids)
+    # Scan for completed jobs
     for array_id in ids_to_scan:
         comp_name = targets[array_id-1]
         if comp_name in completed_srcs:
@@ -116,7 +119,6 @@ def job_loop(targets, status_folder, src_beam_map, active_ids, active_ms, remain
             if array_id in active_ids:
                 print('Completed {}  (#{}) concurrency {}'.format(
                     comp_name, array_id, len(active_ids)))
-                total_concurrency += len(active_ids)
                 active_ids.remove(array_id)
                 tgt_ms = src_beam_map[comp_name]
                 for ms in tgt_ms:
@@ -128,21 +130,33 @@ def job_loop(targets, status_folder, src_beam_map, active_ids, active_ms, remain
             remaining_array_ids.remove(array_id)
             continue
 
+    # Scan for jobs to start
+    for array_id in remaining_array_ids:
+        comp_name = targets[array_id-1]
+        if comp_name in completed_srcs:
+            continue
+
         tgt_ms = src_beam_map[comp_name]
         if tgt_ms & active_ms:
             continue
-        for ms in tgt_ms:
-            active_ms.add(ms)
-        active_ids.add(array_id)
-        # print ('+++ ' + str(active_ids))
-        print('Starting {} (#{}) concurrency {} ms: {}'.format(
-            comp_name, array_id, len(active_ids), tgt_ms))
-        # run_os_cmd('./make_askap_abs_cutout.sh {} {}'.format(array_id, status_folder))
-        run_os_cmd('./start_job.sh {}'.format(array_id))
-    return total_concurrency
+
+        if len(active_ids) < concurrency_limit:
+            rate_limited = False
+            for ms in tgt_ms:
+                active_ms.add(ms)
+            active_ids.add(array_id)
+            # print ('+++ ' + str(active_ids))
+            print('Starting {} (#{}) concurrency {} ms: {}'.format(
+                comp_name, array_id, len(active_ids), tgt_ms))
+            # run_os_cmd('./make_askap_abs_cutout.sh {} {}'.format(array_id, status_folder))
+            run_os_cmd('./start_job.sh {}'.format(array_id))
+        elif not rate_limited:
+            rate_limited = True
+            print (' rate limit of {} applied'.format(concurrency_limit))
+    return len(active_ids)
 
 
-def produce_all_cutouts(targets, status_folder, src_beam_map, delay, max_loops=500):
+def produce_all_cutouts(targets, status_folder, src_beam_map, delay, concurrency_limit, max_loops=500):
     remaining_array_ids = list(range(1, len(targets)+1))
     active_ms = set()
     active_ids = set()
@@ -156,7 +170,7 @@ def produce_all_cutouts(targets, status_folder, src_beam_map, delay, max_loops=5
         print("\nLoop #{}, completed {} remaining {}".format(
             i, len(completed_srcs), len(remaining_array_ids)))
         total_concurrency += job_loop(targets, status_folder, src_beam_map, active_ids, active_ms, remaining_array_ids,
-                                      completed_srcs)
+                                      completed_srcs, concurrency_limit)
         if len(remaining_array_ids) > 0:
             time.sleep(delay)
 
@@ -167,7 +181,7 @@ def produce_all_cutouts(targets, status_folder, src_beam_map, delay, max_loops=5
         raise Exception(msg)
     else:
         print('\nCompleted processing in {} loops with average concurrency {:.2f}'.format(
-            i, total_concurrency/len(targets)))
+            i, total_concurrency/i))
 
 
 def main():
@@ -180,12 +194,11 @@ def main():
 
     # Prepare the run
     targets, image_params = read_image_params(args.filename)
-
     src_beam_map = build_map(image_params)
 
     # Run through the processing
     produce_all_cutouts(targets, args.status_folder,
-                        src_beam_map, args.delay, max_loops=args.max_loops)
+                        src_beam_map, args.delay, args.concurrency_limit, max_loops=args.max_loops)
 
     # Report
     end = time.time()
