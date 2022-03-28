cab5249516d5f039adace1678d0497ac46c87965
James Dempsey <james.dempsey@csiro.au>
2022-03-28 22:29:01 +1100

    Make list_beams.sh portable

--- list_beams.sh
+++ list_beams.sh
@@ -1,25 +1,15 @@
 #!/usr/bin/bash
-##### Select resources #####
-#PBS -N ASKAP_Abs_Prep
-#PBS -l select=1:ncpus=1
-#PBS -l place=vscatter
-#PBS -l walltime=1:00:00
-##### Queue #####
-#PBS -q smallmem
-##### Mail Options #####
-# Send an email at job start, end and if aborted
-#PBS -m a
-##### Change to your working directory #####
-cd /avatar/jdempsey/abs_cutouts
+# Produce a list of the beam measurement sets for an sbid, along with their beam centres
+# Measurement sets are located based on the location listed for the sbid in data_loc.csv in the working  directory
 
-date
+if [ $# -lt 1 ]; then
+  echo "Usage: ${0} sbid"
+  exit 1
+fi
 
-# If not run in PBS, get the sbid as the second param
-if [ -z "${SBID}" ]; then SBID=${1}; fi
+SBID=${1}
 export SBID
-
-#  Show list of CPUs you ran on, if you're running under PBS
-if [ -n "$PBS_NODEFILE" ]; then cat $PBS_NODEFILE; fi
+SCRIPTDIR=`dirname "$0"`
 
 ##### Execute Program #####
-casa -c list_beams.py
+casa -c ${SCRIPTDIR}/list_beams.py
